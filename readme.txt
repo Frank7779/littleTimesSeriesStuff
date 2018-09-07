63c816c88dd20c575f97b2018069d2245cbcded6
Klaus <klaus.kahlfuss@A1.net>
2018-09-07 06:58:05 +0200

    09/072018 - Updated script, data and readme

--- readme.txt
+++ readme.txt
@@ -1,5 +1,7 @@
 Once during my years at university (MSc in Economics ... hell yeah) I started to use R instead of software like SPSS etc.
 The main benefit of scripting in R for me is the clear arranged structure and the traceability of code. Just like this little example I uploaded:
 
-The data itself is stored in the csv-file called R.csv.
+The data was taken back in 2016 from Bureau of Economic Analysisi (BEA) for my own economic analysis (marxian theory meets modern data ...). If more information about the actual data is needed, feel free to contact me (cause for the code/script it's not that imported I guess).
+
+The data itself is stored in the csv-file called timesSeries1_R.csv.
 The acutal R script imports the data needed from the csv-file, does a calculation, creates time series and plots the relevant results.
--- timeSeries1.R
+++ timeSeries1.R
@@ -4,43 +4,48 @@ rm(list=ls())
 cat("\014")
 
 #IMPORT
+#The R.csv (which is uploaded in this repository too) contains the needed data. No all columns are needed, so the "NA" indicates which data was actually imported.
+#Real growth rates of the US GDP in percentage
 dgdp <- read.csv(
-  "PATH TO FILE R.csv",
+  "PATH TO timesSeries1_R.csv",
   header=FALSE,
   colClasses=c(NA, "NULL", "NULL", "NULL", "NULL")
 )
-
+#Capital (capital stock) in Billion US Dollar
 capital <- read.csv(
-  "PATH TO FILE R.csv",
+  "PATH TO timesSeries1_R.csv",
   header=FALSE,
   colClasses=c("NULL", NA, "NULL", "NULL", "NULL")
 )
-
+#Profit in Billion US Dollar
 profit <- read.csv(
-  "PATH TO FILE R.csv",
+  "PATH TO timeSeries1_R.csv",
   header=FALSE,
   colClasses=c("NULL", "NULL", "NULL", "NULL", NA)
 )
 
-#Calculation
+#CALCULATION
+#Rate of profit euqals profit divided by capital
 rate <- (profit/capital*100)
 
-#Time series
+#TIME SERIES
+#This creates a time series which starts back in 1948 and ends 2014 (so 67 values for each time series)
 dgdp_ts <- ts(dgdp, freq=1, start=1948)
 capital_ts <- ts(capital, freq=1, start=1948)
 profit_ts <- ts(profit, freq=1, start=1948)
 rate_ts <- ts(rate, freq=1, start=1948)
 
 #PLOTS
+#These following plots are not the basic ones which R can produce. I edited the font (so it goes along with the paper I used them in), colours as well as style of both axes.
 #Profit
 par(mar=c(4, 4, 4, 4), family="serif", ps=12)
 plot(
   profit_ts, type="l", col="black", lwd=c(1.5),
   ylim=c(0,1500), xlim=c(1948, 2014), 
-  main="Profit 1948-2014",
+  main="Profit in the USA 1948-2014",
   ylab="", xlab=""
 )
-mtext("Profit in Billion Dollar", side=2, line=2.5)
+mtext("Profit in Billion US Dollar", side=2, line=2.5)
 mtext("Year", side=1, line=2.5)
 
 #Capital
@@ -48,10 +53,10 @@ par(mar=c(4, 4, 4, 4), family="serif", ps=12)
 plot(
   capital_ts, type="l", col="black", lwd=c(1.5),
   ylim=c(0,15000), xlim=c(1948, 2014), 
-  main="Capital 1948-2014",
+  main="Capital in the USA 1948-2014",
   ylab="", xlab=""
 )
-mtext("Capital in Billion Dollar", side=2, line=2.5)
+mtext("Capital in Billion US Dollar", side=2, line=2.5)
 mtext("Year", side=1, line=2.5)
 
 #Rate of profit
@@ -59,7 +64,7 @@ par(mar=c(4, 4, 4, 4), family="serif", ps=12)
 plot(
   rate_ts, type="l", col="black", lwd=c(1.5),
   ylim=c(6,16), xlim=c(1948, 2014), 
-  main="Rate of profit 1948-2014",
+  main="Rate of profit in the USA 1948-2014",
   ylab="", xlab=""
 )
 mtext("Rate of profit in percentage", side=2, line=2.5)
@@ -70,18 +75,19 @@ par(mar=c(4, 4, 4, 4), family="serif", ps=12)
 plot(
   dgdp_ts, type="l", col="black", lwd=c(1.5),
   ylim=c(-3,9), xlim=c(1948, 2014), 
-  main="Real growth rate of GDP 1948-2014",
+  main="Real growth rate of GDP of the USA 1948-2014",
   ylab="", xlab=""
 )
 mtext("Real growth rate of GDP in percentage", side=2, line=2.5)
 mtext("Year", side=1, line=2.5)
 
 #Comparison of DGDP and rate of profit
+#This plot is slightly different than the previous ones, because it shows to different time series in one plot, so I had to edit three axis instead of just two as usual.
 par(mar=c(4, 4, 4, 4), family="serif", ps=12)
 plot(
   rate_ts, type="l", col="black", axes=F, lwd=c(1.5),
   ylim=c(6,16),
-  main="Comparion of rate of profit and real growth rate of GDP 1948-2014",
+  main="Comparion of rate of profit and real growth rate of GDP (both of USA) 1948-2014",
   ylab="", xlab=""
 )
 axis(2, ylim=c(4,15), las=0)
